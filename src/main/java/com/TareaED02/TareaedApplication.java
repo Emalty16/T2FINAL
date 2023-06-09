7f41ecae659d59d21c4f97c03012bebe2b7b5960
Emalty16 <vinimadrigalb@gmail.com>
2023-06-08 18:12:46 -0600

    Tarea 02 NUEVO

---
+++ src/main/java/com/TareaED02/TareaedApplication.java
@@ -0,0 +1,13 @@
+package com.TareaED02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaedApplication.class, args);
+	}
+
+}
